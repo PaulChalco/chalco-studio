f31b90e81f2690434b714c5eb2b20bfaaca9871b
Paul Chalco <174530286+PaulChalco@users.noreply.github.com>
2026-07-28 13:23:39 -0600

    Fase G5-Fix: Meta Tags optimizados para buenas prácticas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chalco-lab.netlify.app',
+  site: 'https://chalco-labs.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   }
